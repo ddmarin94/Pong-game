180a6dd8b7243444994168d1e951cfdbd4842326
David Marín <ddmarin94@gmail.com>
2018-05-04 18:14:19 +0200

    working

--- pong.js
+++ pong.js
@@ -10,6 +10,23 @@ class Paddles {
     this.position = new Vector()
     this.size = new Vector(paddleWidth, paddleHeight)
   }
+
+  get left() {
+    return this.position.x - (this.position.x / 2)
+  }
+
+  get right() {
+    return this.position.x + (this.position.x / 2)
+  }
+
+  get top() {
+    return this.position.y - (this.position.y / 2)
+  }
+
+  get bottom() {
+    return this.position.y + (this.position.y / 2)
+  }
+
 }
 
 class Ball extends Paddles {
@@ -51,6 +68,14 @@ window.addEventListener('DOMContentLoaded', () => {
   function update(dt) {
     ball.position.x += ball.speed.x * dt
     ball.position.y += ball.speed.y * dt
+
+    //si sale de los limites del cambas
+    if(ball.position.x < 0 || ball.position.x > (canvas.width - ball.size.x)) {
+      ball.speed.x = -ball.speed.x
+    }
+    if(ball.position.y < 0 || ball.position.y > (canvas.height - ball.size.y)) {
+      ball.speed.y = -ball.speed.y
+    }
 
     context.fillStyle = '#000'
     context.fillRect(0, 0, canvas.width, canvas.height)
